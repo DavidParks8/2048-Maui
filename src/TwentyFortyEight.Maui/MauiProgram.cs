8d6843eb2c9476679960d517e1668514d3b5928b
David Parks <DavidParks8@outlook.com>
2025-12-24 10:35:42 -0800

    fix icon rendering inside buttons

--- src/TwentyFortyEight.Maui/MauiProgram.cs
+++ src/TwentyFortyEight.Maui/MauiProgram.cs
@@ -13,15 +13,7 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
-        builder
-            .UseMauiApp<App>()
-            .UseMauiCommunityToolkit()
-            .UseSkiaSharp()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+        builder.UseMauiApp<App>().UseMauiCommunityToolkit().UseSkiaSharp().ConfigureFonts(_ => { });
 
 #if DEBUG
         builder.Logging.AddDebug();
