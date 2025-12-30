bf560345d75373220246d580a9c405a8a46e00ca
David Parks <DavidParks8@outlook.com>
2025-12-29 21:48:47 -0800

    change app id

--- src/TwentyFortyEight.Maui/Services/PlatformAchievementIds.cs
+++ src/TwentyFortyEight.Maui/Services/PlatformAchievementIds.cs
@@ -12,19 +12,19 @@ public static class PlatformAchievementIds
     // iOS Game Center IDs
     public static class iOS
     {
-        public const string LeaderboardId = "com.davidparks.twentyfourtyeight.highscores";
-        public const string Achievement_Tile128 = "com.davidparks.twentyfourtyeight.tile128";
-        public const string Achievement_Tile256 = "com.davidparks.twentyfourtyeight.tile256";
-        public const string Achievement_Tile512 = "com.davidparks.twentyfourtyeight.tile512";
-        public const string Achievement_Tile1024 = "com.davidparks.twentyfourtyeight.tile1024";
-        public const string Achievement_Tile2048 = "com.davidparks.twentyfourtyeight.tile2048";
-        public const string Achievement_Tile4096 = "com.davidparks.twentyfourtyeight.tile4096";
-        public const string Achievement_FirstWin = "com.davidparks.twentyfourtyeight.firstwin";
-        public const string Achievement_Score10000 = "com.davidparks.twentyfourtyeight.score10000";
-        public const string Achievement_Score25000 = "com.davidparks.twentyfourtyeight.score25000";
-        public const string Achievement_Score50000 = "com.davidparks.twentyfourtyeight.score50000";
+        public const string LeaderboardId = "com.dappermagna.2048.highscores";
+        public const string Achievement_Tile128 = "com.dappermagna.2048.tile128";
+        public const string Achievement_Tile256 = "com.dappermagna.2048.tile256";
+        public const string Achievement_Tile512 = "com.dappermagna.2048.tile512";
+        public const string Achievement_Tile1024 = "com.dappermagna.2048.tile1024";
+        public const string Achievement_Tile2048 = "com.dappermagna.2048.tile2048";
+        public const string Achievement_Tile4096 = "com.dappermagna.2048.tile4096";
+        public const string Achievement_FirstWin = "com.dappermagna.2048.firstwin";
+        public const string Achievement_Score10000 = "com.dappermagna.2048.score10000";
+        public const string Achievement_Score25000 = "com.dappermagna.2048.score25000";
+        public const string Achievement_Score50000 = "com.dappermagna.2048.score50000";
         public const string Achievement_Score100000 =
-            "com.davidparks.twentyfourtyeight.score100000";
+            "com.dappermagna.2048.score100000";
     }
 
     // Future: Xbox achievement IDs for Windows
